1b84c000d71c907011ac70f4cadb1816b870f53c
Remus Daniel Heghedus <hrdaniel4877@gmail.com>
2017-11-07 17:38:20 -0500

    Install react-sparklines charts

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,4 +1,10 @@
-export default function (state=null, action) {
+import {FETCH_WEATHER} from '../actions/index';
+
+export default function (state=[], action) {
 	console.log('Action AFTER redux-promise Middleware processing is a Data Object:\n', action);
+	switch (action.type) {
+		case FETCH_WEATHER:
+			return [action.payload.data, ...state];
+	};	
 	return state;
 };
